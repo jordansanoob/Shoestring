6ddc20ed8b364dc9ef34210b31909ad6a6b9004c
jordansanoob <jhabbatiello@gmail.com>
2017-10-05 23:32:19 -0400

    Add files via upload

--- createTables.sql
+++ createTables.sql
@@ -1,72 +1,85 @@
 use jha2135;
 
 drop table IF EXISTS users;
+drop table if exists department;
+drop table if exists brand;
 drop table IF EXISTS inventory;
 drop table if exists cart;
 drop table if exists purchased;
-drop table if exists brand;
-drop table if exists department;
 
+CREATE TABLE users
+(
+	userId INT NOT NULL,
+	lastName varchar(255) NOT NULL,
+	firstName varchar(255) NOT NULL,
+	userEmail varchar(320) NOT NULL,
+	userPassword varchar(64) NOT NULL,
+	PRIMARY KEY (userId)
+)
+ENGINE=INNODB;
 
+CREATE TABLE department
+(
+	departmentId varchar(3) NOT NULL,
+	departmentName varchar(48) NOT NULL,
+	departmentManager varchar(48) NOT NULL,
+	PRIMARY KEY (departmentId)
+)
+ENGINE=INNODB;
 
-CREATE TABLE users (
-					userId INT NOT NULL,
-					lastName varchar(255) NOT NULL,
-					firstName varchar(255) NOT NULL,
-					email varchar(320) NOT NULL,
-					password varchar(64) NOT NULL,
-					PRIMARY KEY (userId)
-)ENGINE=INNODB;
+CREATE TABLE brand
+(
+	brandId varchar(4) NOT NULL,
+	brandName varchar(255) not null,
+	brandAddress varchar(64) NOT NULL,
+	phoneNumber varchar(12),
+	repFirstName varchar(16) NOT NULL,
+	repLastName varchar(16) NOT NULL,
+	PRIMARY KEY (brandId)
+)
+ENGINE=INNODB;
 
-CREATE TABLE department (
-					departmentId INT NOT NULL,
-					departmentName varchar(48) NOT NULL,
-					departmentManager varchar(48) NOT NULL,
-					PRIMARY KEY (departmentId)
-) ENGINE=INNODB;
+CREATE TABLE inventory
+(
+	itemId varchar(7) NOT NULL,
+	itemName varchar(32) NOT NULL,
+	price DECIMAL(19,4) NOT NULL,
+	gender VARCHAR(1),
+	departmentId varchar(3) NOT NULL,
+	brandId varchar(4),
+	PRIMARY KEY (itemId),
+	FOREIGN KEY (departmentId) references department(departmentId),
+	FOREIGN KEY (brandId) references brand(brandId)
+)
+ENGINE=INNODB;
 
-CREATE TABLE brand (
-					brandId INT NOT NULL,
-					address varchar(64) NOT NULL,
-					phoneNumber INT NOT NULL,
-					repFirstName varchar(16) NOT NULL,
-					repLastName varchar(16) NOT NULL,
-					PRIMARY KEY (brandId)
-) ENGINE=INNODB;
+CREATE TABLE cart
+(
 
-CREATE TABLE inventory (
-					itemId INT NOT NULL,
-					price DECIMAL(19,4) NOT NULL,
-					gender VARCHAR(1),
-					departmentId INT NOT NULL,
-					brandId INT NOT NULL,
-					PRIMARY KEY (itemId),
-					FOREIGN KEY (departmentId) references department(departmentId),
-					FOREIGN KEY (brandId) references brand(brandId)
-)ENGINE=INNODB;
+	cartId INT NOT NULL,
+	quantity INT NOT NULL,
+	userId INT NOT NULL,
+	itemId varchar(7) NOT NULL,
+	PRIMARY KEY (cartId),
+	FOREIGN KEY (userId) REFERENCES users(userId),
+	FOREIGN KEY (itemId) REFERENCES inventory(itemId)
+)
+ENGINE=INNODB;
 
-CREATE TABLE cart (
-					cartId INT NOT NULL,
-					quantity INT NOT NULL,
-					userId INT NOT NULL,
-					itemId INT NOT NULL,
-					PRIMARY KEY (cartId),
-					FOREIGN KEY (userId) REFERENCES users(userId),
-					FOREIGN KEY (itemId) REFERENCES inventory(itemId)
-) ENGINE=INNODB;
+CREATE TABLE purchased
+(
+	purchaseId INT NOT NULL,
+	userId INT NOT NULL,
+	itemId varchar(7) NOT NULL,
+	quantity INT NOT NULL,
+	purchaseDate date NOT NULL,
+	deliveryDate date NOT NULL,
+	PRIMARY KEY (purchaseId),
+	FOREIGN KEY (userId) REFERENCES users(userId),
+	FOREIGN KEY (itemId) REFERENCES inventory(itemId)
 
-CREATE TABLE purchased (
-					purchaseId INT NOT NULL,
-					userId INT NOT NULL,
-					itemId INT NOT NULL,
-					quantity INT NOT NULL,
-					purchaseDate date NOT NULL,
-					deliveryDate date NOT NULL,
-					PRIMARY KEY (purchaseId),
-					FOREIGN KEY (userId) REFERENCES users(userId),
-					FOREIGN KEY (itemId) REFERENCES inventory(itemId)
-					
-) ENGINE=INNODB;
+)
+ENGINE=INNODB;
 
 
 
--- populateTables.sql
+++ populateTables.sql
@@ -1,17 +1,98 @@
+delete from users;
+delete from department;
+delete from brand;
+delete from inventory;
+delete from cart;
+delete from purchased;
+
 insert into users
-values();
+values(1000, "admin", "admin", "admin@shoestring.com", "password");
+insert into users
+values(2000, "Sebastian", "Stan", "s.stan@yahoo.com", "sstanpass");
+insert into users
+values(3000, "Winston", "Devine", "w.devine@gmail.com", "wdevinepass");
+insert into users
+values(4000, "Francesca", "Wilder", "f.wilder@uncw.edu", "fwilderpass");
+insert into users
+values(5000, "Jamie", "Lane", "j.lane@aol.com", "jlanepass");
+insert into users
+values(6000, "Parker", "Cotterfield", "p.cotterfield@bing.com", "pcotterfieldpass");
+insert into users
+values(7000, "Melissa", "Ferrero", "m.ferrero@gmail.com", "mferreropass");
 
 insert into department
-values();
+values("MUS", "Music", "Lennon");
+insert into department
+values("CLO", "Clothing", "Coach");
+insert into department
+values("BKS", "Books", "Fitzgerald");
+insert into department
+values("ELE", "Electronics", "Tesla");
+insert into department
+values("HME", "Home/Kitchen", "Pilgrim");
+insert into department
+values("ATO", "Automobile", "Ford");
 
 insert into brand
-values();
+values("NIKE", "Nike", "999 Highpoint Dr.", "555-555-5555", "John", "Jones");
+insert into brand
+values("ADID", "Adidas", "384 Anchor Pl.", "154-658-9998", "Cathy", "Hunter");
+insert into brand
+values("FARB", "Farberware", "293 Indian Summer Ct.", "652-873-1545", "Edna", "Walkerfield");
+insert into brand
+values("HLGR", "Tommy Hilfiger", "500 Passport Rd.", "960-327-8136", "Tommy", "Hilfiger");
+insert into brand
+values("SONY", "Sony", "739 Lasso Ln.", "671-118-4843", "Cooper", "Bannon");
+insert into brand
+values("TSBA", "Toshiba", "88 Bobblehead Dr.", "561-284-9989", "Stevia", "Cooks");
+insert into brand
+values("PTGA", "Patagonia", "293 Ironsleeve Ct.", "654-946-3333", "Marcus", "Vez");
 
 insert into inventory
-values();
-
-insert into cart
-values();
-
-insert into purchased
-values();
+values("NS-8945", "Men's Nike Shoes", 64.99, "0", "CLO", "NIKE");
+insert into inventory
+values("NS-8345", "Women's Nike Shoes ", 34.99, "1", "CLO", "NIKE");
+insert into inventory
+values("NS-7365", "Kid's Nike Shoes", 59.99, "0", "CLO", "NIKE");
+insert into inventory
+values("FB-2839", "Fiction Book", 12.99, null, "BKS", null);
+insert into inventory
+values("NB-2229", "Non-fiction Book", 9.99, null, "BKS", null);
+insert into inventory
+values("MB-2589", "Mystery Book", 14.99, null, "BKS", null);
+insert into inventory
+values("OJ-9384", "Outdoor Jacket", 129.99, "1", "CLO", "PTGA");
+insert into inventory
+values("OH-9234", "Outdoor Hat", 24.99, "0", "CLO", "PTGA");
+insert into inventory
+values("OP-9384", "Outdoor Pants", 79.99, "1", "CLO", "PTGA");
+insert into inventory
+values("AS-9003", "Adidas Socks 3-pack", 12.99, "0", "CLO", "ADID");
+insert into inventory
+values("A2-9023", "Adidas Socks 3-pack", 12.99, "1", "CLO", "ADID");
+insert into inventory
+values("AT-9293", "Adidas T-Shirt", 29.99, "0", "CLO", "ADID");
+insert into inventory
+values("KB-3293", "Knife Block", 69.99, null, "HME", "FARB");
+insert into inventory
+values("CB-1193", "Cutting Board", 9.99, null, "HME", "FARB");
+insert into inventory
+values("SS-3993", "Spatula Set", 19.99, null, "HME", "FARB");
+insert into inventory
+values("TV-9009", "Sony Television", 349.99, null, "ELE", "SONY");
+insert into inventory
+values("SB-9088", "Sound Bar", 139.99, null, "ELE", "SONY");
+insert into inventory
+values("PS-0004", "Playstation 4", 399.99, null, "ELE", "SONY");
+insert into inventory
+values("HP-1138", "Headphones", 49.99, null, "ELE", "TSBA");
+insert into inventory
+values("HS-2938", "Home Speakers", 99.99, null, "ELE", "TSBA");
+insert into inventory
+values("RC-2001", "Remote Control", 14.99, null, "ELE", "TSBA");
+insert into inventory
+values("HJ-2039", "Hilfiger Jeans", 59.99, "0", "CLO", "HLGR");
+insert into inventory
+values("WH-9939", "Winter Hat", 59.99, "0", "CLO", "HLGR");
+insert into inventory
+values("HB-1024", "Hilfiger Belt", 18.99, "0", "CLO", "HLGR");
