02bc3c31d457bdf494cb098bdea555c625b647bb
mclaurinpd <vanhalen5228>
2017-11-29 12:44:04 -0500

    fixed purchased procedure

--- sql/purchaseProc.sql
+++ sql/purchaseProc.sql
@@ -5,6 +5,7 @@ labelLeave:BEGIN
 	
 	DECLARE idItem varchar(255);
 	DECLARE qty int;
+	DECLARE itemPrice decimal(19,4);
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
 		BEGIN
 			SELECT "Error: Purchase procedure did not work." AS ERROR;
@@ -15,13 +16,15 @@ labelLeave:BEGIN
 	WHILE (SELECT DISTINCT 1 FROM cart WHERE userId = id LIMIT 1) DO
 		SET idItem := (SELECT itemId FROM cart WHERE userId=id LIMIT 1);
 		SET qty := (SELECT quantity FROM cart WHERE userId=id LIMIT 1);
-		IF (qty * (SELECT price FROM inventory WHERE itemId=idItem) > (SELECT wallet FROM users WHERE userId = id)) THEN 
+		SET itemPrice := (SELECT price FROM inventory WHERE itemId=idItem);
+		IF (qty * itemPrice > (SELECT wallet FROM users WHERE userId = id)) THEN 
 			ROLLBACK;
 			SELECT "Insufficient funds" AS ERROR;
 			LEAVE labelLeave;
 		END IF;
 		INSERT INTO purchased (userId, itemId, quantity, purchaseDate)
 					values    (id, idItem, qty, CURDATE());
+		UPDATE users SET wallet = wallet - (qty * itemPrice) WHERE userId = id;
 		DELETE FROM cart WHERE itemId = idItem AND userId = id;
 	END WHILE;
 	COMMIT;
